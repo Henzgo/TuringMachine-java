74e3bf8f49db905a34346d89fb860652ba93239d
Henry Eschenmoser <henry.eschenmoser@gmail.com>
2024-05-13 19:02:07 +0200

    First idea TuringMachine class

--- app/src/main/java/turingmachine/TuringMachine.java
+++ app/src/main/java/turingmachine/TuringMachine.java
@@ -2,12 +2,14 @@ package turingmachine;
 
 public class TuringMachine {
     private Tape tape;
+    private TransitionTable transitionTable;
     private String input;
 
 
     public TuringMachine(String input) {
         this.input = input;
         tape = new Tape(input);
+        this.transitionTable = new TransitionTable();
     }
 
     
